7505d28de8b749d169ac747bb5c46f213bd3a83b
Pedro Sierra <pedroluis1280@gmail.com>
2025-07-01 18:12:22 -0500

    Add endpoint to get user created URLs

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RequestWithUser } from '../auth/request-with-user.interface';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('urls')
+  getUrls(@Req() req: RequestWithUser) {
+    return this.userService.getUserUrls(req.user.sub);
+  }
+}
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async getUserUrls(userId: string) {
+    return this.prisma.url.findMany({
+      where: { userId },
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+}
