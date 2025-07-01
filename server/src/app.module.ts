a23be008a93c1cd739a79dac46d07b9b4f78c7fa
Pedro Sierra <pedroluis1280@gmail.com>
2025-07-01 17:39:27 -0500

    Troubleshoot prisma

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UrlModule } from './url/url.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UrlModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    UrlModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
