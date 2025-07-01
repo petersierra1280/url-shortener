e174fb7b40c065e4858bde15b43990c0c753a4c7
Pedro Sierra <pedroluis1280@gmail.com>
2025-07-01 16:43:07 -0500

    Update prettier settings for .ts files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UrlModule } from './url/url.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, UrlModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
